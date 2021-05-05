df67f326f0a4d9a94d7229de5e11d4bd19f52be1
Olubo Ifeoluwa <ifeoluwaolubo@gmail.com>
2021-05-05 01:28:17 +0100

    Updated settings file

--- config/settings.py
+++ config/settings.py
@@ -83,11 +83,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': env.dj_db_url('DATABASE_URL')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
